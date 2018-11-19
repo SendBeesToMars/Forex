f868744ab099f70ee3e0fbf3ed31b302f3f66051
Ed <datum1337@gmail.com>
2018-11-19 08:15:54 +0000

    update

--- ForexServer/Server.py
+++ ForexServer/Server.py
@@ -6,28 +6,35 @@ import asyncio
 import random
 import websockets
 import python_forex_quotes  # this lib was build on an older version of python some fucntions dont work.
+
 #  Change urllib.urlopen( to urllib.request.urlopen(
 # also change #import urllib to #import urllib.request
 
 
 client = python_forex_quotes.ForexDataClient("iPLcRg1tsNOa5zw7ni1LQG53IBKjkVo6")
 if client.marketIsOpen():
-    print(client.getQuotes(["EURUSD"]))
+    print("Maket is Open")
+    marketStatus = True
 else:
     print("Pools closed")
+    marketStatus = False
 
 
 async def websoc(websocket, path):
-    while True:
+    while marketStatus:
         item1 = ("EURUSD price " + (str(client.getQuotes(["EURUSD"])[0].get("price"))))
         item2 = ("EURGBP price " + (str(client.getQuotes(["EURGBP"])[0].get("price"))))
 
         await websocket.send(item1)
         await websocket.send(item2)
+        print("hello there")
         await asyncio.sleep(random.random() * 3)
 
-        name = await websocket.recv()  #if this is enabled, the server only send data only twice
+        name = await websocket.recv()  # TODO: if this is enabled, the server only send data only twice. recv() raises a
+        # ConnectionClosed exception when the client disconnects
+        # stops the send function after the JavaScript send a response and python writes it to console
         print("Received: " + name)
+
 
 start_server = websockets.serve(websoc, "localhost", 42069)
 
